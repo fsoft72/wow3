64720abee94c42c1d85e823437aa10cce6bc888a
fsoft <fabio.rotondo@gmail.com>
2026-02-14 08:34:21 +0100

    ADD: copy to os3 script

---
+++ scripts/copy-to-os3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ARCHIVE="/ramdisk/wow3.tar.bz2"
+DEST_DIR="~/dev/web/os3-website-cf/tools/wow3"
+
+tar cfj $ARCHIVE  css js index.html
+
+cd $DEST_DIR
+
+if [ $? -ne 0 ]; then
+    echo "Failed to change directory to $DEST_DIR"
+    exit 1
+fi
+
+rm -Rf *
+tar xfj $ARCHIVE
+
+echo "File copied"
